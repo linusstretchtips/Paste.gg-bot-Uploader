2a2f7102fde2e9801158e26cb203d98158ad6fc1
linusstretchtips <grzeskibitz@gmail.com>
2021-02-23 18:14:42 +0000

    ty crusher

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Paste.gg-bot-Uploader
 A discord bot that uplaods the content of a file to paste.gg
+
+## Setup
+1) Clone the repo `git clone https://github.com/linusstretchtips/Paste.gg-bot-Uploader.git`
+2) Install the needed packages `npm install`
+3) Rename `config.js.template` to `config.js`
+4) Edit the needed variables in `config.js`
+5) Start the bot `node .`
--- index.js
+++ index.js
@@ -7,18 +7,18 @@ const axios = require("axios");
 const add = require('date-fns/add')
 const format = require('date-fns/format')
 
-const { 
-  discord_bot_token,
-  pastegg_secret_key,
-  message_embed_colour,
-  message_embed_title,
-  message_description,
-  message_expiration,
-  message_error,
-  pastegg_title,
-  pastegg_description,
-  pastegg_file_name,
-  pastegg_visability
+const {
+    discord_bot_token,
+    pastegg_secret_key,
+    message_embed_colour,
+    message_embed_title,
+    message_description,
+    message_expiration,
+    message_error,
+    pastegg_title,
+    pastegg_description,
+    pastegg_file_name,
+    pastegg_visability
 } = require('./config')
 
 const contentTypes = ['application/json', 'text/plain', 'text/yaml'];
@@ -26,48 +26,48 @@ const pasteGG = new PasteGG(pastegg_secret_key)
 
 //Tell console were up and running
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', async message => {
     if (message.channel.type !== 'text' || message.author.bot) return; //check if the message isnt a file
     if (!message.attachments) return; //check if the message doesnt have any attatchments
     for (const attachment of message.attachments.values()) {
-      let contentType = mimeType.lookup(attachment.url); 
-      if (!contentTypes.some(type => contentType === type)) continue;
-      try {
-      const date = add(new Date(), { //Get the current date and add a week ontop. see this for more https://date-fns.org/v2.17.0/docs/add#examples
-        weeks: 1
-      })
+        let contentType = mimeType.lookup(attachment.url);
+        if (!contentTypes.some(type => contentType === type)) continue;
+        try {
+            const date = add(new Date(), { //Get the current date and add a week ontop. see this for more https://date-fns.org/v2.17.0/docs/add#examples
+                weeks: 1
+            })
 
-      let content = await axios.get(attachment.url); //let axios grab the url
-      var result = await pasteGG.post({ //now we post the paste. check here for more info https://www.npmjs.com/package/paste.gg
-        name: pastegg_title, // Optional
-        description: pastegg_description, // Optional
-        visibility: pastegg_visability, //Optional
-        expires: date.toISOString(), //Optional
-        files: [{
-          name: pastegg_file_name, // Optional
-          content: {
-            format: "text",
-            value: content.data
-          }
-        }]
-      })
+            let content = await axios.get(attachment.url); //let axios grab the url
+            var result = await pasteGG.post({ //now we post the paste. check here for more info https://www.npmjs.com/package/paste.gg
+                name: pastegg_title, // Optional
+                description: pastegg_description, // Optional
+                visibility: pastegg_visability, //Optional
+                expires: date.toISOString(), //Optional
+                files: [{
+                    name: pastegg_file_name, // Optional
+                    content: {
+                        format: "text",
+                        value: content.data
+                    }
+                }]
+            })
 
-      var json = result.result; //go into the correct json bracket
-      var expiration = format(date, 'dd-MM-yyyy') 
+            var json = result.result; //go into the correct json bracket
+            var expiration = format(date, 'dd-MM-yyyy')
 
-      const embed = new Discord.MessageEmbed() //Get the embed ready
-      .setColor(message_embed_colour)
-      .setTitle(message_embed_title)
-      .setDescription(message_description + json.url + message_expiration + expiration)
-      await message.channel.send(embed) //Send the embeded message
+            const embed = new Discord.MessageEmbed() //Get the embed ready
+                .setColor(message_embed_colour)
+                .setTitle(message_embed_title)
+                .setDescription(message_description + json.url + message_expiration + expiration)
+            await message.channel.send(embed) //Send the embeded message
 
-    } catch (e) {
-    await message.channel.send(message_error)
-    console.log(e)
-   }
-  }
+        } catch (e) {
+            await message.channel.send(message_error)
+            console.log(e)
+        }
+    }
 });
 client.login(discord_bot_token);
